4cf21776391a0b7b80dce2ded4014b4ce2782989
Yusuf Tran <yusuf.tran@cognolink.com>
2014-03-07 20:53:12 +0000

    Tidy up

--- ldap/ldap-add-user.sh
+++ ldap/ldap-add-user.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-die() { echo $* 1>&2 ; exit 1 ; }
-# ldapsearch -x -v -H ldaps://***REMOVED***.***REMOVED***.com:10636 -D "cn=ssp,ou=services,o=cl" -b "ou=users,o=cl" -w <pass>
-#git archive --remote=git@***REMOVED***.***REMOVED***.com:infrastructure/scripts.git master:repos/dnb --format=tar description | tar xf -
-# ldapsearch -x -H ldaps://***REMOVED***.***REMOVED***.com:10636 -D "cn=ldap_mgt,ou=services,o=cl" -b "ou=users,o=cl" -w <pass> '(&(objectClass=inetOrgPerson)(uid=***REMOVED***))'
-
+# Add a new user to our apache DS LDAP directory
+# Need to ensure we trim and validate fields/input from rundeck
 # hopefully we won't have 2 users with the same names!!!
 
 # SETTINGS #
@@ -15,14 +12,16 @@ BINDUSER="cn=ldap_mgt,ou=services,o=cl"
 BINDPASS=
 USERPASS=
 USERPASSHASH=$(slappasswd -h {sha} -s $USERPASS)
-
 FNAME=Joe # aka givenName
 SNAME=Bloggs
 DISPLAYNAME="$FNAME $SNAME"
 DN="cn=$DISPLAYNAME,$BASEURL"
-# lower case names - tr [A-Z] [a-z]
 LUID="$(echo "$FNAME.$SNAME" | tr [A-Z] [a-z])"
-EMAIL=$LUID@***REMOVED***.com
+EMAILDOMAIN="@***REMOVED***.com"
+EMAIL=$LUID@$EMAILDOMAIN
+
+# FUNCTIONS #
+die() { echo $* 1>&2 ; exit 1 ; }
 
 # check if user exists
 ldapsearch $LDAPOPTIONS $LDAPURL \
