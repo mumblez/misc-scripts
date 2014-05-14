19c9ef1bca7595bb0b4a202669f8b13e999ce7f4
Yusuf Tran <yusuf.tran@cognolink.com>
2014-05-14 11:49:57 +0100

    fix spacing

--- watchdogs/pt-kill-long-query.sh
+++ watchdogs/pt-kill-long-query.sh
@@ -33,7 +33,7 @@ case "$1" in
       --match-user '(?i-xsm:sqluser)' \
       --log "$KILLLOG" \
       --print \
-      --execute-command "( echo To: it-monitoring@***REMOVED***.com; echo From: admin@***REMOVED***.com; echo Subject: 'Long Query Killed'; echo; cat /var/log/  mysql-kill.log) | sendmail -t; cat $KILLLOG >> $KILLLOGARCHIVE; > $KILLLOG" \
+      --execute-command "( echo To: it-monitoring@***REMOVED***.com; echo From: admin@***REMOVED***.com; echo Subject: 'Long Query Killed'; echo; cat /var/log/mysql-kill.log) | sendmail -t; cat $KILLLOG >> $KILLLOGARCHIVE; > $KILLLOG" \
       --kill-query
 
     RETVAL=$?
