068d56698b2f03dcb96ba9d9d2caf8bd568af4d3
Yusuf Tran <yusuf.tran@cognolink.com>
2014-07-31 15:18:10 +0100

    Working POC, cleans up old releases (keeps 4 plus current one), does composer install vs update, moved deploy key to somewhere where apache can access, all running under apache user

--- deployments/symfony-poc.sh
+++ deployments/symfony-poc.sh
@@ -10,7 +10,7 @@ PHP="/usr/local/php55/bin/php"
 PHP_FPM="/etc/init.d/php5-fpm"
 SITE_USER="www-data"
 SITE_GROUP="www-data"
-DEPLOY_KEY="/***REMOVED***/keys/cl_deploy"
+DEPLOY_KEY="/***REMOVED***/web/cl_deploy"
 GIT_REPO="@option.repository_url@"
 GIT_OPTIONS="--recursive" #(optional, if using submodules)
 GIT_BRANCH="@option.branch@"
@@ -23,25 +23,31 @@ DEPLOY_DIR="${DEPLOY_ROOT}/${TIMESTAMP}"
 WEBROOT="/***REMOVED***/www/symfony2"
 APP_ENV="@option.environment@"
 COMPOSER="${SYMFONY_ROOT}/binaries/composer.phar"
-COMPOSER_OPTIONS="--no-interaction --working-dir=$DEPLOY_DIR"
+#COMPOSER_OPTIONS="--no-interaction --working-dir=$DEPLOY_DIR"
+COMPOSER_OPTIONS="--no-progress --no-interaction"
 CONSOLE="$DEPLOY_DIR/app/console"
-CONSOLE_OPTIONS="--env=$APP_ENV"
+CONSOLE_OPTIONS="--env=dev"
 SYMFONY_PARAMS_FILE="$DEPLOY_DIR/app/config/parameters.$APP_ENV.yml"
+TMP_SCRIPT=$(mktemp /tmp/deploy-XXX.sh)
+chmod +x $TMP_SCRIPT
+chown $SITE_USER:$SITE_GROUP $TMP_SCRIPT
+
 
 # add production specific settings
 if [[ "$APP_ENV" == "prod" ]]; then
 	COMPOSER_OPTIONS="$COMPOSER_OPTIONS --no-dev --optimize-autoloader"
-	CONSOLE_OPTIONS="$CONSOLE_OPTIONS --no-debug"
+	CONSOLE_OPTIONS="--env=prod --no-debug"
 fi
 
 ### Validation ###
-echo "Validation checks...."
-id "$SITE_USER" && echo "Site user - $SITE_USER found" || die "ERROR: User $SITE_USER does not exist"
-grep "$SITE_GROUP" /etc/group && echo "Site group - $SITE_GROUP found" || die "ERROR: Group $SITE_GROUP does not exist"
-[ -e "$DEPLOY_KEY" ] && echo "Deployment key found - $DEPLOY_KEY"  || die "ERROR: Deployment key - $DEPLOY_KEY - does not exist"
+echo "INFO: Validation checks...."
+id "$SITE_USER" && echo "INFO: Site user - $SITE_USER found" || die "ERROR: User $SITE_USER does not exist"
+grep "$SITE_GROUP" /etc/group && echo "INFO: Site group - $SITE_GROUP found" || die "ERROR: Group $SITE_GROUP does not exist"
+[ -e "$DEPLOY_KEY" ] && echo "INFO: Deployment key found - $DEPLOY_KEY"  || die "ERROR: Deployment key - $DEPLOY_KEY - does not exist"
 [ -e "$PHP_FPM" ] || die "ERROR: $PHP_FPM does not exist"
 [ -e "$PHP" ] || die "ERROR: $PHP does not exist"
 which curl 2>&1 >/dev/null || die "ERROR: curl is not installed"
+
 
 # Download latest composer.phar #
 cd "$SYMFONY_ROOT/binaries"
@@ -111,24 +117,39 @@ chmod 777 "$DEPLOY_DIR/app/logs" -R
 chmod 777 "$DEPLOY_DIR/app/cache" -R
 
 cd "$DEPLOY_DIR"
-sudo -u "$SITE_USER" "$PHP" "$COMPOSER" $COMPOSER_OPTIONS self-update && echo "INFO: Composer - self updated" || die "ERROR: Composer: self update failed"
-sudo -u "$SITE_USER" "$PHP" "$COMPOSER" $COMPOSER_OPTIONS update && echo "INFO: Composer - updated" || die "ERROR: Composer: update failed"
-sudo -u "$SITE_USER" "$PHP" "$CONSOLE" cache:clear "$CONSOLE_OPTIONS" && echo "INFO: Console - cache cleared" || die "ERROR: Console: cache clear failed"
-sudo -u "$SITE_USER" "$PHP" "$CONSOLE" assetic:dump "$CONSOLE_OPTIONS" && echo "INFO: Console - dump assets" || die "ERROR: Console: dumping assets failed"
-sudo -u "$SITE_USER" "$PHP" "$CONSOLE" assetic:install "$CONSOLE_OPTIONS" && echo "INFO: Console - install assets" || die "ERROR: Console: installing assets failed"
+sudo -u "$SITE_USER" "$PHP" "$COMPOSER" self-update && echo "INFO: Composer - self updated" || die "ERROR: Composer: self update failed"
+#sudo -u "$SITE_USER" "$PHP" "$COMPOSER" $COMPOSER_OPTIONS install && echo "INFO: Composer - updated" || die "ERROR: Composer: update failed"
+
+
+#sudo -u "$SITE_USER" "$PHP" "$COMPOSER" $COMPOSER_OPTIONS update && echo "INFO: Composer - updated" || die "ERROR: Composer: update failed"
+
+cat > ${TMP_SCRIPT} <<EOF
+#!/bin/bash
+eval \$(ssh-agent -s)
+ssh-add $DEPLOY_KEY
+"$PHP" "$COMPOSER" $COMPOSER_OPTIONS install
+kill \$SSH_AGENT_PID
+exit 0
+EOF
+
+sudo -u "$SITE_USER" /bin/bash ${TMP_SCRIPT} || die "ERROR: Composer: update failed"
+
+sudo -u "$SITE_USER" "$PHP" "$CONSOLE" cache:clear $CONSOLE_OPTIONS && echo "INFO: Console - cache cleared" || die "ERROR: Console: cache clear failed"
+sudo -u "$SITE_USER" "$PHP" "$CONSOLE" assetic:dump $CONSOLE_OPTIONS && echo "INFO: Console - dump assets" || die "ERROR: Console: dumping assets failed"
+sudo -u "$SITE_USER" "$PHP" "$CONSOLE" assets:install $CONSOLE_OPTIONS && echo "INFO: Console - install assets" || die "ERROR: Console: installing assets failed"
 
 # make cache and log dir writeable
 chmod 775 "$DEPLOY_DIR/app/logs" -R
 chmod 775 "$DEPLOY_DIR/app/cache" -R
 
 #symlink deploy_dir to real_dir
-ln -snf "$DEPLOY_DIR" "$REAL_DIR" && echo "Symlinked deployment release directory - $DEPLOY_DIR to $REAL_DIR" || die "ERROR: Symlinking deployment release directory - $DEPLOY_DIR to $REAL_DIR failed"
+ln -snf "$DEPLOY_DIR" "$REAL_DIR" && echo "INFO: Symlinked deployment release directory - $DEPLOY_DIR to $REAL_DIR" || die "ERROR: Symlinking deployment release directory - $DEPLOY_DIR to $REAL_DIR failed"
 
 # Set permission to symlink (incase apache only follows symlinks with same owner)
 chown -h "$SITE_USER":"$SITE_GROUP" "$REAL_DIR" -R
 
 #symlink web***REMOVED***
-ln -snf "$REAL_DIR/web" "$WEBROOT" && echo "Symlinked deployment release web ***REMOVED*** - $REAL_DIR to $WEBROOT" || die "ERROR: Symlinking deployment release web***REMOVED*** - $REAL_DIR to $WEBROOT failed"
+ln -snf "$REAL_DIR/web" "$WEBROOT" && echo "INFO: Symlinked deployment release web ***REMOVED*** - $REAL_DIR to $WEBROOT" || die "ERROR: Symlinking deployment release web***REMOVED*** - $REAL_DIR to $WEBROOT failed"
 
 # Set permission to web***REMOVED*** (incase apache only follows symlinks with same owner)
 chown -h "$SITE_USER":"$SITE_GROUP" "$WEBROOT"
@@ -144,5 +165,20 @@ chown -h "$SITE_USER":"$SITE_GROUP" "$WEBROOT"
 /etc/init.d/apache2 restart
 
 ### no more steps
-echo "Deployment suceeded!"
+echo "INFO: Deployment suceeded!"
+
+# CLEANUP 
+echo "INFO: Cleaning up..."
+# Clearing old releases
+CURRENT_RELEASE=$(basename $(readlink $REAL_DIR))
+RECENT_RELEASES=$(ls -tr1 /srv/symfony/releases | grep -vE "shared|$CURRENT_RELEASE" | tail -n4)
+for OLD_RELEASE in $(ls -tr1 "$DEPLOY_ROOT" | grep -vE "shared|$CURRENT_RELEASE"); do
+	if ! echo "$OLD_RELEASE" | grep -q "$RECENT_RELEASES"; then
+		echo "INFO: Deleted old release - $OLD_RELEASE"
+		rm -rf "${DEPLOY_ROOT}/${OLD_RELEASE}"
+	fi
+done
+
+# Delete tmp script
+rm -f "$TMP_SCRIPT"
 exit 0
