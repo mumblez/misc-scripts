621627b5f133d713e5856110ff35a43014867157
Yusuf Tran <yusuf.tran@cognolink.com>
2015-04-10 18:30:13 +0100

    -add informative output

--- backup/rundeck-and-ca-backup.sh
+++ backup/rundeck-and-ca-backup.sh
@@ -22,6 +22,7 @@ ZB_KEY="/srv/zbackup"
 ZB_INFO="/srv/info"
 
 # run local backups
+echo "INFO: Backing up Rundeck and CA..."
 rd-jobs list -p Everything -f "$JOBS" --format yaml &>/dev/null
 /usr/bin/mysqldump --defaults-file=/***REMOVED***/.my.cnf -B rundeck > "$DB"
 tar -cf "$ARCHIVE" "$JOBS" "$DB" --remove-files &>/dev/null
@@ -29,6 +30,7 @@ tar --append --file="$ARCHIVE" "$RD_PROJECTS"
 tar --append --file="$ARCHIVE" "$CA_DIR"
 
 # pull zbackup key and info from backup
+echo "INFO: pull zbackup keys"
 rsync -ar -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "${SSH_USER}"@"${REMOTE_SERVER}":"${ZB_INFO_REMOTE}" "$ZB_INFO"
 rsync -ar -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "${SSH_USER}"@"${REMOTE_SERVER}":"${ZB_KEY_REMOTE}" "$ZB_KEY"
 
@@ -39,17 +41,22 @@ rsync -ar -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "${SSH_USER}"@"${REMOTE
 ln -snf "$ZB_INFO" "${ZB_REPO}/"
 
 # rsync index from backup
+echo "INFO: sync remote zbackup index..."
 rsync -ar -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "${SSH_USER}"@"${REMOTE_SERVER}":"${ZB_REPO_REMOTE}/index" "${ZB_REPO}/"
 
 # run backup through zbackup
+echo "INFO: creating zbackup backup..."
 cat "$ARCHIVE" | "$ZB_BIN" --password-file "$ZB_KEY" backup "${ZB_REPO}/backups/$(basename $ARCHIVE)" &>/dev/null
 
 # rsync backup, bundles, index to backup
+echo "INFO: uploading zbackup data"
 rsync -ar -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "${ZB_REPO}/backups/" "${SSH_USER}"@"${REMOTE_SERVER}":"${ZB_REPO_REMOTE}/backups/${ZB_APP_NAME}/daily"
 rsync -ar -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "${ZB_REPO}/bundles/" "${SSH_USER}"@"${REMOTE_SERVER}":"${ZB_REPO_REMOTE}/bundles"
 rsync -ar -e "ssh -i $SSH_KEY" --rsync-path="sudo rsync" "${ZB_REPO}/index/" "${SSH_USER}"@"${REMOTE_SERVER}":"${ZB_REPO_REMOTE}/index"
 
 # delete backup files, bundles and index 
+echo "INFO: cleaning up..."
+set -x
 rm -f "$ARCHIVE"
 rm -rf "${ZB_REPO}/backups/*"
 rm -rf "${ZB_REPO}/bundles/*"
@@ -58,3 +65,5 @@ rm -rf "${ZB_REPO}/index/*"
 # shred keys and backups
 shred -u "$ZB_INFO"
 shred -u "$ZB_KEY"
+set +x
+echo "INFO: RD and CA backup successful."
