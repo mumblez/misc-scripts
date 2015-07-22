9dc6c36517513cccc48da1344813a5917b7fbb8a
Yusuf Tran <yusuf.tran@cognolink.com>
2015-07-22 14:42:10 +0100

    Cleanup tag logic, init repo if doesn't exist, cleanup variable quoting, add branch / tag logic, rsync locally if app1 else ssh to deploy / webistrano and add start and stop time.

--- deployments/svn-migration/deb-checkout-build-distribute.sh
+++ deployments/svn-migration/deb-checkout-build-distribute.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# INITIALISE THE REPO'S FOR FIRST TIME BEFORE RUNNING (e.g. /var/lib/webistrano/git)
+# So for multi choice projects we don't have to query, map and pass in git url's
+# Temporary until we move to all symfony
 
 die() { echo $* 1>&2 ; exit 1 ; }
 ### Settings ###
@@ -13,8 +16,9 @@ BUILDSCRIPT="/***REMOVED***/bin/package"
 APP_ENVIRONMENT="@option.environment@"
 GIT_URL="git@***REMOVED***.***REMOVED***.com"
 GIT_NAMESPACE="***REMOVED***"
-TAG_PREFIX="release-"
+#TAG_PREFIX="release-"
 PKG_REPO_URL="rundeck@bishop" # change to new repo when testing
+START_TIME=$(date)
 
 ### Validation ###
 echo "Validation checks...."
@@ -28,64 +32,77 @@ which git >/dev/null 2>&1 || die "ERROR: git is not installed"
 git_pull () {
   PROJECT="$1"
   # if first time then git clone
-  if [ ! -d "$PROJECTS_DIR/$PROJECT" ]; then
+  if [ ! -d "${PROJECTS_DIR}/${PROJECT}" ]; then
     cd "$PROJECTS_DIR"
     echo "INFO: Initial clone of $PROJECT..."
-    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git clone " || die "ERROR: Git clone from $GIT_REPO failed"
+    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git clone ${GIT_URL}:${GIT_NAMESPACE}/${PROJECT}.git" || die "ERROR: Git clone from $GIT_REPO failed"
   else
-    cd "$PROJECTS_DIR/$PROJECT" || die "ERROR: project does not exist @ $PROJECTS_DIR/$PROJECT"
-    echo "INFO: Refreshing $PROJECT...."
+    cd "${PROJECTS_DIR}/${PROJECT}"
+    echo "INFO: Refreshing ${PROJECT}..."
     #echo "INFO: origin URLs:"
     #git remote -v
-    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git fetch" || die "ERROR: Git clone from $GIT_REPO failed"
-    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git fetch --tags" || die "ERROR: Git clone from $GIT_REPO failed"
-    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git fetch --all" || die "ERROR: Git clone from $GIT_REPO failed"
+    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git fetch" || die "ERROR: Git fetch from $GIT_REPO failed"
+    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git fetch --tags" || die "ERROR: Git fetch from $GIT_REPO failed"
+    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git fetch --all" || die "ERROR: Git fetch from $GIT_REPO failed"
   fi
 }
 
 # Checkout latest release
 git_checkout () {
   PROJECT="$1"
-  cd "$PROJECTS_DIR/$PROJECT" || die "ERROR: project does not exist @ $PROJECTS_DIR/$PROJECT"
+  cd "${PROJECTS_DIR}/${PROJECT}"
   # if yii, offspring or sphinx then don't checkout tag, just checkout master
   if [[ "$PROJECT" == "yii" || "$PROJECT" == "sphinx" || "$PROJECT" = "offspring" ]]; then
     echo "INFO: checking out master branch for $PROJECT...."
     ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git checkout $GIT_OPTIONS master &>/dev/null" || die "ERROR: Could not checkout master branch for $PROJECT"
   else
-    echo "INFO: checking out release ${TAG_PREFIX}${RELEASE} for $PROJECT...."
-    ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git checkout $GIT_OPTIONS ${TAG_PREFIX}${RELEASE} &>/dev/null" || die "ERROR: Could not checkout tag:$RELEASE for $PROJECT"
+    if [[ "$APP_ENVIRONMENT" != "prod" ]]; then
+      if [ -z "$RELEASE" ]; then
+        SPRINT_BRANCH=$(git branch -r | cut -d'/' -f2 | grep 'sprint' | sort -rV | head -n1)
+      else
+        SPRINT_BRANCH="$RELEASE"
+      fi
+      echo "INFO: checking out latest sprint branch - $SPRINT_BRANCH for $PROJECT on $APP_ENVIRONMENT..."
+      ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git checkout $GIT_OPTIONS $SPRINT_BRANCH &>/dev/null" || die "ERROR: Could not checkout $SPRINT_BRANCH for $PROJECT"
+    else
+      echo "INFO: checking out release ${TAG_PREFIX}${RELEASE} for $PROJECT..."
+      ssh-agent bash -c "ssh-add $DEPLOY_KEY &>/dev/null && git checkout $GIT_OPTIONS ${RELEASE} &>/dev/null" || die "ERROR: Could not checkout tag:$RELEASE for $PROJECT"
+    fi  
   fi
   # If there are issues, as an alternative, blow away folder and do a new pull / clone
 }
 
 build_and_dist () {
-    PROJECT="$1"
-	cd "$PROJECTS_DIR/$PROJECT/build" || die "ERROR: project build directory does not exist @ $PROJECTS_DIR/$PROJECT"
-	PACKAGE=$(grep Package control | awk {'print $2'})
-	echo "INFO: building and distributing $PROJECT..."
-	$BUILDSCRIPT $APP_ENVIRONMENT &>/dev/null || die "ERROR: failed to build $PROJECT"
-	#$DISTRIBUTESCRIPT $PACK*.deb $APP_ENVIRONMENT &>/dev/null || die "ERROR: failed to distribute $PROJECT"
-	echo "INFO: distributing to $i package repo..."
-	# enable agent forwarding
-    #scp $PACK*.deb ${PKG_REPO_URL}:/srv/deb_repository/dists-poc/${APP_ENVIRONMENT}/main/binary-amd64 &>/dev/null || die "ERROR: failed to distribute $PROJECT"
-    rsync -e "ssh" --rsync-path="sudo rsync" $PACK*.deb ${PKG_REPO_URL}:/srv/deb_repository/dists-poc/${APP_ENVIRONMENT}/main/binary-amd64 &>/dev/null || die "ERROR: failed to distribute $PROJECT"
-    #scp $PACK*.deb ${PKG_REPO_URL}:/srv/deb_repository/dists/${APP_ENVIRONMENT}/main/binary-amd64 &>/dev/null || die "ERROR: failed to distribute $PROJECT"
-	rm -rf "$PROJECTS_DIR/$PROJECT/build/debian" || echo "WARN: failed to delete debian folder from build directory!"
+  PROJECT="$1"
+  cd "${PROJECTS_DIR}/${PROJECT}/build" || die "ERROR: project build directory does not exist @ ${PROJECTS_DIR}/${PROJECT}"
+  PACKAGE=$(grep Package control | awk {'print $2'})
+  echo "INFO: building and distributing ${PROJECT}..."
+  "$BUILDSCRIPT" "$APP_ENVIRONMENT" &>/dev/null || die "ERROR: failed to build $PROJECT"
+  echo "INFO: distributing to $i package repo..."
 
+  ################### CHANGE 'dists-poc' to 'dists' after testing!!!!!  ############
+
+  # deploy / webistrano
+  #rsync -e "ssh" --rsync-path="sudo rsync" $PACK*.deb "${PKG_REPO_URL}:/srv/deb_repository/dists-poc/${APP_ENVIRONMENT}/main/binary-amd64" &>/dev/null || die "ERROR: failed to distribute $PROJECT"
+  # app1 / bishop
+  rsync -e $PACK*.deb "/srv/deb_repository/dists-poc/${APP_ENVIRONMENT}/main/binary-amd64" &>/dev/null || die "ERROR: failed to distribute $PROJECT"
+
+  rm -rf "$PROJECTS_DIR/$PROJECT/build/debian" || echo "WARN: failed to delete debian folder from build directory!"
 }
 
 ### MAIN ###
 
 # Refresh sources from git
-for project in $PROJECTS; do git_pull $project; done
+for project in $PROJECTS; do git_pull "$project"; done
 
 # Checkout tag / release
-for project in $PROJECTS; do git_checkout $project; done
-
+for project in $PROJECTS; do git_checkout "$project"; done
 
 # Build and publish project to bishop
 for project in $PROJECTS; do build_and_dist "$project"; done
 
 ### no more steps
+echo "INFO: Started - $START_TIME"
+echo "INFO: Ended   - $(date)"
 echo "INFO: Build succeeded"
 exit 0
