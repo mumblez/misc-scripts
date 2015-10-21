5bbdb85f60de0042df17852cda82ac39c85341d5
Yusuf Tran <yusuf.tran@cognolink.com>
2015-10-21 17:01:41 +0100

    Add option to update git repositories from git and also restart apache and php-fpm services

--- deployments/svn-migration/deploy-offspring-application.sh
+++ deployments/svn-migration/deploy-offspring-application.sh
@@ -19,6 +19,7 @@ HOST_REPO="@option.host_repo@"
 UAT_FE="@option.uat_frontend@"
 PROJECTS="@option.projects@"
 TAG="@option.tag@"
+SVN2GIT="@option.svn2git@"
 # Rundeck job uuid references
 JOB_BUILD_PKG="f41a878b-d2e5-4323-83ca-7dcd844a604d"
 JOB_UPDATE_REPO="4aa08aad-4b62-4b97-9417-daf377a0c2e2"
@@ -27,7 +28,8 @@ JOB_SQL_EXEC="84425e20-1d3c-4a72-b38f-b9adda5cc685" # For intranet and/or websit
 JOB_EVENTLOGTRIGGER_GEN="fc803349-bbbf-4924-8618-7eb5d3f62518"
 JOB_EVENTLOGTRIGGER_XFER="1b58f880-6c07-46c4-84c7-53525e008e06"
 JOB_EVENTLOGTRIGGER_RUN="bb2c2b76-4901-46f0-9719-6a17184a058b"
-
+JOB_SVN2GIT="876850e5-0646-4a27-8e0d-4b3d9ec99cac"
+JOB_SERVICE_RESTART="abdc55a5-5c66-487b-b9f2-6286d50490bd"
 
 
 ## Mappings - front (web) and back (db) ends
@@ -80,6 +82,13 @@ case "$ENVIRONMENT" in
 	#	;;
 esac
 
+if [[ "$SVN2GIT" == "yes" ]]; then
+	echo "INFO: Converting and updating git repositories from SVN..."
+	run -i "$JOB_SVN2GIT" -f
+fi
+[ "$?" != 0 ] && die "ERROR: Error with svn 2 git conversion!"
+
+
 echo "INFO: Deploying for environment $ENVIRONMENT"
 
 # Build debian packages from source (pass in list of projects)
@@ -104,6 +113,12 @@ run -i "$JOB_INSTALL_PKG" -f -- -host "$HOST_WEB" -projects "$PROJECTS"
 
 [ "$?" != 0 ] && die "ERROR: Error installing debian package(s)"
 echo "INFO: Succesfully installed debian package(s)"
+
+run -i "$JOB_SERVICE_RESTART" -f -- -host "$HOST_WEB" -service "php52-fpm"
+[ "$?" != 0 ] && die "ERROR: restarting php52-fpm service"
+run -i "$JOB_SERVICE_RESTART" -f -- -host "$HOST_WEB" -service "apache2"
+[ "$?" != 0 ] && die "ERROR: restarting apache2 service"
+echo "INFO: Successfully restarted apache and php-fpm services."
 
 # Set permissions on /***REMOVED*** - hack until can figure out where offspring logs gets its permissions from
 ssh -o StrictHostKeyChecking=no rundeck@${HOST_WEB} sudo chown www-data:www-data /***REMOVED*** -R
